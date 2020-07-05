0b92e7e1fea9a5134d6177ded127cdd663a329f6
sheensekai <sheensekai@ya.ru>
2020-07-05 21:36:43 +0300

    Move User class to entities package

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.repository;
 
-import com.example.model.User;
+import com.example.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
