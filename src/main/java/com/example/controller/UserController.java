d4e194335e7e7838403fa03902cd95f9b1ff5195
sheensekai <sheensekai@ya.ru>
2020-07-06 00:24:06 +0300

    Add getUserById method

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.example.controller;
 
 import com.example.dto.UserDTO;
+import com.example.exception.ResourceNotFoundException;
 import com.example.model.UserModel;
 import com.example.repository.UserRepository;
 import com.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
@@ -25,5 +23,18 @@ public class UserController {
 
         UserModel newUser = new UserModel(id, userName, email, passwordHash);
         return this.userService.addUser(newUser);
+    }
+
+    @GetMapping("/users/{userId}")
+    public UserDTO getUserById(@PathVariable(value = "userId") Integer userId)
+        throws ResourceNotFoundException {
+        UserModel user = this.userService.getUserById(userId);
+
+        long id = user.getId();
+        String userName = user.getUserName();
+        String email = user.getEmail();
+        long passwordHash = user.getPasswordHash();
+
+        return new UserDTO(id, userName, email, passwordHash);
     }
 }
