2bae340a21e861b0f3b1a3d0e00f8515b82d9525
sheensekai <sheensekai@ya.ru>
2020-07-06 18:12:08 +0300

    Add existsByGender in GenderRepository

--- src/main/java/com/example/repository/GenderRepository.java
+++ src/main/java/com/example/repository/GenderRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GenderRepository extends JpaRepository<Gender, Integer> {
+    boolean existsByGender(String gender);
 }
