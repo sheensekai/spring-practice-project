0da26819fa74f7bc078fa174e3e5f526d738a3e0
sheensekai <sheensekai@ya.ru>
2020-07-07 13:07:48 +0300

    Add UserInfoDTO(UserInfoModel)

--- src/main/java/com/example/dto/UserInfoDTO.java
+++ src/main/java/com/example/dto/UserInfoDTO.java
@@ -1,5 +1,6 @@
 package com.example.dto;
 
+import com.example.model.UserInfoModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -12,5 +13,13 @@ public class UserInfoDTO {
     private String lastName;
     private String gender;
     private long birthDate;
+
+    public UserInfoDTO(UserInfoModel userInfoModel) {
+        this.userId = userInfoModel.getUserId();
+        this.firstName = userInfoModel.getFirstName();
+        this.lastName = userInfoModel.getLastName();
+        this.gender = userInfoModel.getGenderEnum().toString().toLowerCase();
+        this.birthDate = userInfoModel.getBirthDate();
+    }
 
 }
