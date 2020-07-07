768101c7fbfe411fd9b872aaac1803bb2200579f
sheensekai <sheensekai@ya.ru>
2020-07-07 10:39:23 +0300

    Add UserInfoRepository

---
+++ src/main/java/com/example/repository/UserInfoRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+
+import com.example.entities.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+}
