37e759dda47982ff3cf58e109a332d978563ebbe
sheensekai <sheensekai@ya.ru>
2020-07-07 14:06:33 +0300

    Add GenderModel.genderId, update its constructors

--- src/main/java/com/example/model/GenderModel.java
+++ src/main/java/com/example/model/GenderModel.java
@@ -8,17 +8,23 @@ import lombok.Data;
 
 @Data
 public class GenderModel {
+    private int genderId;
     private GenderEnum gender;
 
-    public GenderModel(String gender) {
+    public GenderModel(String gender, int genderId) {
         this.gender = GenderEnum.findEnum(gender);
         if (this.gender == null) {
             throw new GenderEnumDoesntExistException("Enum for " + gender + " doesn't exist");
         }
+
+        this.genderId = genderId;
     }
 
     public GenderModel(GenderDTO genderDTO) {
-        this(genderDTO.getGender());
+        this(genderDTO.getGender(), genderDTO.getGenderId());
     }
-    public GenderModel(Gender gender) { this(gender.getGender()); }
+
+    public GenderModel(Gender gender) {
+        this(gender.getGender(), gender.getGenderId());
+    }
 }
