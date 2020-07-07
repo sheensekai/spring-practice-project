9c15572488ed3796b774dfd810e9466d3e05220e
sheensekai <sheensekai@ya.ru>
2020-07-07 12:59:57 +0300

    Add UserInfoService.updateUserInfo & do renaming
    Renaming is for making variables names more clear and correct

--- src/main/java/com/example/service/UserInfoService.java
+++ src/main/java/com/example/service/UserInfoService.java
@@ -18,18 +18,18 @@ public class UserInfoService {
     @Autowired
     private GenderRepository genderRepository;
 
-    public void addUserInfo(UserInfoModel userInfo) {
-        if (this.userInfoRepository.existsById(userInfo.getUserId())) {
-            throw new ResourceAlreadyExistsException("UserInfo with userId " + userInfo.getUserId() + " already exists");
+    public void addUserInfo(UserInfoModel userInfoModel) {
+        if (this.userInfoRepository.existsById(userInfoModel.getUserId())) {
+            throw new ResourceAlreadyExistsException("UserInfo with userId " + userInfoModel.getUserId() + " already exists");
         }
 
-        GenderEnum newUserGenderEnum = userInfo.getGenderEnum();
+        GenderEnum newUserGenderEnum = userInfoModel.getGenderEnum();
         String genderString = newUserGenderEnum.toString().toLowerCase();
         Gender newUserGender = this.genderRepository.findByGender(genderString)
                 .orElseThrow( () -> new ResourceNotFoundException("Gender " + newUserGenderEnum.toString().toLowerCase() + " doesn't exist"));
 
         int genderId = newUserGender.getId();
-        UserInfo newUserInfo = new UserInfo(userInfo, genderId);
+        UserInfo newUserInfo = new UserInfo(userInfoModel, genderId);
 
         this.userInfoRepository.save(newUserInfo);
     }
@@ -38,7 +38,20 @@ public class UserInfoService {
         UserInfo userInfo = this.userInfoRepository.findById(userId)
                 .orElseThrow( () -> new ResourceNotFoundException("UserInfo with userId " + userId + " doesn't exist"));
 
-        String gender = this.genderRepository.findById(userInfo.getGenderId()).get().getGender();
-        return new UserInfoModel(userInfo, gender);
+        String genderString = this.genderRepository.findById(userInfo.getGenderId()).get().getGender();
+        return new UserInfoModel(userInfo, genderString);
+    }
+
+    public void updateUserInfo(UserInfoModel userInfoModel) {
+        if (!this.userInfoRepository.existsById(userInfoModel.getUserId())) {
+            throw new ResourceNotFoundException("UserInfo with userId " + userInfoModel.getUserId() + " doesn't exist");
+        }
+
+        String genderString = userInfoModel.getGenderEnum().toString().toLowerCase();
+        Gender gender = this.genderRepository.findByGender(genderString)
+                .orElseThrow( () -> new ResourceNotFoundException("Gender " + genderString + " doesn't exist"));
+
+        UserInfo newUserInfo = new UserInfo(userInfoModel, gender.getId());
+        this.userInfoRepository.save(newUserInfo);
     }
 }
