0462d87b404222b9361a4b5160cd3b650b5e4fa0
sheensekai <sheensekai@ya.ru>
2020-07-07 14:04:12 +0300

    Add GenderDTO(GenderModel)

--- src/main/java/com/example/dto/GenderDTO.java
+++ src/main/java/com/example/dto/GenderDTO.java
@@ -1,10 +1,17 @@
 package com.example.dto;
 
+import com.example.model.GenderModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class GenderDTO {
+    private int genderId;
     private String gender;
+
+    public GenderDTO(GenderModel genderModel) {
+        this.genderId = genderModel.getGenderId();
+        this.gender = genderModel.getGender().toString().toLowerCase();
+    }
 }
