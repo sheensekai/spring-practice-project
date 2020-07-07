81d02fb4d828e871a7c63b0841687f741842f0cd
sheensekai <sheensekai@ya.ru>
2020-07-07 15:05:08 +0300

    Modify UserInfoModel with lombok

--- src/main/java/com/example/model/UserInfoModel.java
+++ src/main/java/com/example/model/UserInfoModel.java
@@ -12,26 +12,33 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserInfoModel {
     private int userId;
-    @NonNull private String firstName;
-    @NonNull private String lastName;
+
+    @NonNull
+    private String firstName;
+
+    @NonNull
+    private String lastName;
     private GenderEnum genderEnum;
-    @NonNull private long birthDate;
+
+    @NonNull
+    private long birthDate;
+
+    private void initGenderEnum(String genderString) {
+        this.genderEnum = GenderEnum.findEnum(genderString);
+        if (this.genderEnum == null) {
+            throw new GenderEnumDoesntExistException("Enum for " + genderString + " doesn't eixst");
+        }
+    }
 
     public UserInfoModel(UserInfoDTO userInfoDTO) {
         this(userInfoDTO.getFirstName(), userInfoDTO.getLastName(), userInfoDTO.getBirthDate());
-
-        this.genderEnum = GenderEnum.findEnum(userInfoDTO.getGender());
-        if (this.genderEnum == null) {
-            throw new GenderEnumDoesntExistException("Enum for " + userInfoDTO.getGender() + " doesn't eixst");
-        }
+        this.userId = userInfoDTO.getUserId();
+        this.initGenderEnum(userInfoDTO.getGender());
     }
 
     public UserInfoModel(UserInfo userInfo, String gender) {
         this(userInfo.getFirstName(), userInfo.getLastName(), userInfo.getBirthDate());
-
-        this.genderEnum = GenderEnum.findEnum(gender);
-        if (this.genderEnum == null) {
-            throw new GenderEnumDoesntExistException("Enum for " + gender + " doesn't eixst");
-        }
+        this.userId = userInfo.getUserId();
+        this.initGenderEnum(gender);
     }
 }
