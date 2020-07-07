02f2c8b49a980598e07ff8112c3bd6cf0908d2c3
sheensekai <sheensekai@ya.ru>
2020-07-07 14:02:50 +0300

    Make GenderService.addGender return GenderModel instead of void

--- src/main/java/com/example/service/GenderService.java
+++ src/main/java/com/example/service/GenderService.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import com.example.entities.Gender;
 import com.example.exception.ResourceAlreadyExistsException;
+import com.example.exception.ResourceNotFoundException;
 import com.example.model.GenderModel;
 import com.example.repository.GenderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,28 @@ public class GenderService {
     @Autowired
     private GenderRepository genderRepository;
 
-    public void addGender(GenderModel gender) {
+    public GenderModel addGender(GenderModel gender) {
         if (this.existsGender(gender)) {
             throw new ResourceAlreadyExistsException("Gender " + gender.getGender().toString() + " already exists");
         }
 
         Gender newGender = new Gender(gender);
-        this.genderRepository.save(newGender);
+        newGender = this.genderRepository.save(newGender);
+        return new GenderModel(newGender);
+    }
+
+    public GenderModel findGenderByGender(String genderString) {
+        Gender gender = this.genderRepository.findByGender(genderString)
+                .orElseThrow(() -> new ResourceNotFoundException("Gender " + genderString + " doesn't exist"));
+
+        return new GenderModel(gender);
+    }
+
+    public GenderModel findGenderById(int genderId) {
+        Gender gender = this.genderRepository.findById(genderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Gender with id " + genderId + " doesn't exist"));
+
+        return new GenderModel(gender);
     }
 
     public boolean existsGender(GenderModel gender) {
