8f0b920a550395ac887e594b695c00e4f97a063e
sheensekai <sheensekai@ya.ru>
2020-07-07 13:29:40 +0300

    Rename User.id to User.userId

--- src/main/java/com/example/entities/User.java
+++ src/main/java/com/example/entities/User.java
@@ -16,7 +16,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private int userId;
 
     @Column(name = "username", unique = true)
     private String userName;
