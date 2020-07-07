20491dcdeac6057e2a957b8b2439f899e5face8f
sheensekai <sheensekai@ya.ru>
2020-07-07 15:03:57 +0300

    Modify UserInfoDTO with lombok

--- src/main/java/com/example/dto/UserInfoDTO.java
+++ src/main/java/com/example/dto/UserInfoDTO.java
@@ -1,25 +1,32 @@
 package com.example.dto;
 
 import com.example.model.UserInfoModel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserInfoDTO {
 
     private int userId;
+
+    @NonNull
     private String firstName;
+
+    @NonNull
     private String lastName;
+
+    @NonNull
     private String gender;
+
+    @NonNull
     private long birthDate;
 
     public UserInfoDTO(UserInfoModel userInfoModel) {
+        this(userInfoModel.getFirstName(), userInfoModel.getLastName(),
+                userInfoModel.getGenderEnum().toString().toLowerCase(), userInfoModel.getBirthDate());
         this.userId = userInfoModel.getUserId();
-        this.firstName = userInfoModel.getFirstName();
-        this.lastName = userInfoModel.getLastName();
-        this.gender = userInfoModel.getGenderEnum().toString().toLowerCase();
-        this.birthDate = userInfoModel.getBirthDate();
     }
 
 }
