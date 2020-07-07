4aef6815e9fb40825bcce0205d661f8de9880b70
sheensekai <sheensekai@ya.ru>
2020-07-07 15:05:27 +0300

    Modify UserModel with lombok

--- src/main/java/com/example/model/UserModel.java
+++ src/main/java/com/example/model/UserModel.java
@@ -3,25 +3,30 @@ package com.example.model;
 import com.example.dto.UserDTO;
 import com.example.entities.User;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
 public class UserModel {
     private int userId;
+
+    @NonNull
     private String userName;
+
+    @NonNull
     private String email;
+
+    @NonNull
     private long passwordHash;
 
     public UserModel(User user) {
+        this(user.getUserName(), user.getEmail(), user.getPasswordHash());
         this.userId = user.getUserId();
-        this.userName = user.getUserName();
-        this.email = user.getEmail();
-        this.passwordHash = user.getPasswordHash();
     }
 
     public UserModel(UserDTO userDTO) {
+        this(userDTO.getUserName(), userDTO.getEmail(), userDTO.getPasswordHash());
         this.userId = userDTO.getUserId();
-        this.userName = userDTO.getUserName();
-        this.email = userDTO.getEmail();
-        this.passwordHash = userDTO.getPasswordHash();
     }
 }
