d83c341134f24ff446fca5697d553c0c45a91af8
sheensekai <sheensekai@ya.ru>
2020-07-08 13:10:49 +0300

    Add GenderServiceImplTests with some tests

--- src/test/java/com/example/tests/services/GenderServiceImplTests.java
+++ src/test/java/com/example/tests/services/GenderServiceImplTests.java
@@ -1,5 +1,7 @@
 package com.example.tests.services;
 
+import com.example.entities.Gender;
+import com.example.exception.ResourceNotFoundException;
 import com.example.model.GenderModel;
 import com.example.repository.GenderRepository;
 import com.example.service.impl.GenderServiceImpl;
@@ -11,9 +13,9 @@ import org.mockito.Mockito;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GenderServiceImplTests {
     private GenderServiceImpl genderService;
@@ -21,6 +23,7 @@ public class GenderServiceImplTests {
     private static List<String> rightNames;
     private static List<String> wrongNames;
     private static List<GenderModel> genderModelList;
+    private static List<Gender> genderList;
 
     @BeforeAll
     static void init() {
@@ -32,9 +35,14 @@ public class GenderServiceImplTests {
         wrongNames.addAll(Arrays.asList("notmale", "malemale", "fefefefemale", "notfemale", "asdfasdgasd",
                 "femal", "mal", "email", "femalemale"));
 
+        genderList = new ArrayList<>();
+        for (int i = 0; i < rightNames.size(); ++i) {
+            genderList.add(new Gender(i, rightNames.get(i)));
+        }
+
         genderModelList = new ArrayList<>();
         for (int i = 0; i < rightNames.size(); ++i) {
-            genderModelList.add(new GenderModel(rightNames.get(i), i + 1));
+            genderModelList.add(new GenderModel(genderList.get(i)));
         }
     }
 
@@ -47,8 +55,11 @@ public class GenderServiceImplTests {
     public void whenGenderAlreadyExistsReturnsTrue() {
         Mockito.when(genderRepository.existsByGender(Mockito.anyString())).thenReturn(true);
         genderService = new GenderServiceImpl(genderRepository);
+
         for (GenderModel genderModel : genderModelList) {
-            assertTrue(this.genderService.existsGender(genderModel));
+            assertTrue(
+                    this.genderService.existsGender(genderModel),
+                    "Returns false while gender is contained in the repotisotry");
         }
     }
 
@@ -56,8 +67,59 @@ public class GenderServiceImplTests {
     public void whenGenderDoesntExistReturnsFalse() {
         Mockito.when(genderRepository.existsByGender(Mockito.anyString())).thenReturn(false);
         genderService = new GenderServiceImpl(genderRepository);
+
         for (GenderModel genderModel : genderModelList) {
-            assertFalse(this.genderService.existsGender(genderModel));
+            assertFalse(
+                    this.genderService.existsGender(genderModel),
+                    "Returns true while gender is not contained in the repository");
+        }
+    }
+
+    @Test
+    public void whenGenderIsContainedByGenderIdReturnsGenderModel() {
+        for (int i = 0; i < genderList.size(); ++i) {
+            Optional<Gender> optionalGender = Optional.of(genderList.get(i));
+            Mockito.when(genderRepository.findById(Mockito.anyInt())).thenReturn(optionalGender);
+            genderService = new GenderServiceImpl(genderRepository);
+
+            assertEquals(
+                    this.genderService.findGenderByGenderId(i),
+                    genderModelList.get(i),
+                    "Returned genderModel must be equal because they are made of the same Gender");
+        }
+    }
+
+    @Test
+    public void whenGenderIsNotContainedByGenderNameThrowsException() {
+            Mockito.when(genderRepository.findByGender(Mockito.anyString())).thenThrow(new ResourceNotFoundException(""));
+            genderService = new GenderServiceImpl(genderRepository);
+            assertThrows(ResourceNotFoundException.class, () -> genderService.findGenderByGender(rightNames.get(0)));
+    }
+
+    @Test
+    public void whenGenderIsContainedByGenderNameReturnsGenderModel() {
+        for (int i = 0; i < genderList.size(); ++i) {
+            Optional<Gender> optionalGender = Optional.of(genderList.get(i));
+            Mockito.when(genderRepository.findByGender(Mockito.anyString())).thenReturn(optionalGender);
+            genderService = new GenderServiceImpl(genderRepository);
+
+            assertEquals(
+                    this.genderService.findGenderByGender(genderList.get(i).getGenderName()),
+                    genderModelList.get(i),
+                    "Returned genderModel must be equal because they are made of the same Gender");
+        }
+    }
+
+    @Test
+    public void whenAddsNotContainedGenderReturnsNewMadeGender() {
+        for (int i = 0; i < genderList.size(); ++i) {
+            Mockito.when(genderRepository.save(Mockito.any())).thenReturn(genderList.get(i));
+            genderService = new GenderServiceImpl(genderRepository);
+
+            assertEquals(
+                    this.genderService.addGender(genderModelList.get(i)),
+                    new GenderModel(genderList.get(i)),
+                    "Returned genderModel must be equals because they are made of the same Gender");
         }
     }
 
